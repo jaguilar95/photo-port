0913ea0ff53cc6577ff8d959c951ab2bdc6a2701
Leonardo <josue.aguilar1995@gmail.com>
2022-07-30 09:46:57 -0500

    added About tests

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 import React from "react";
-// import ReactDOM from "react-dom/client";
-import { render } from "react-dom";
+import ReactDOM from "react-dom/client";
+// import { render } from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-// const root = render.createRoot(document.getElementById("root"));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-const root = document.getElementById("root"); // <- This is the correct method call for React version 17
-render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  root
+  </React.StrictMode>
 );
+
+// const root = document.getElementById("root"); // <- This is the correct method call for React version 17
+// render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   root
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
